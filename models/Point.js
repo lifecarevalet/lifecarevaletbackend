60bcc42d0e36ae25bf30ae7040fdedd0a62304fa
lifecarevalet <lifecarevalet@gmail.com>
2025-12-11 10:43:07 +0530

    Create Point.js

---
+++ models/Point.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const PointSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    address: { type: String, required: true },
+    ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Point', PointSchema);
